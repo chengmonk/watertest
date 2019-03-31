7d970f5f173f9cbe0bb8f50e23244c643aeed6c6
子俊 程 <a719532792@hotmail.com>
2019-03-31 10:24:42 +0800

    添加ModbusAscii 类

---
+++ 冲水阀水力特性测试机/ModbusAscii.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HslCommunication;
+using System.Windows.Forms;
+namespace 冲水阀水力特性测试机
+{
+    public class ModbusAscii
+    {
+            
+    }
+}
