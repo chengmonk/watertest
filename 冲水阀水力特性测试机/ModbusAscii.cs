93cfe3a6049b965b9c14766013fea251703e277d
子俊 程 <a719532792@hotmail.com>
2019-03-31 10:27:09 +0800

    ModbusAscii中添加读写返回信息的静态方法

--- 冲水阀水力特性测试机/ModbusAscii.cs
+++ 冲水阀水力特性测试机/ModbusAscii.cs
@@ -9,6 +9,86 @@ namespace 冲水阀水力特性测试机
 {
     public class ModbusAscii
     {
-            
+        /// <summary>
+        /// 统一的读取结果的数据解析，显示
+        /// </summary>
+        /// <typeparam name="T">类型对象</typeparam>
+        /// <param name="result">读取的结果值</param>
+        /// <param name="address">地址信息</param>
+        /// <param name="textBox">输入的控件</param>
+        public static void ReadResultRender<T>(OperateResult<T> result, string address, TextBox textBox)
+        {
+            if (result.IsSuccess)
+            {
+                textBox.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + $"[{address}] {result.Content}{Environment.NewLine}");
+            }
+            else
+            {
+                MessageBox.Show(DateTime.Now.ToString("[HH:mm:ss] ") + $"[{address}] Read Failed {Environment.NewLine}Reason：{result.ToMessageShowString()}");
+            }
+        }
+
+        /// <summary>
+        /// 统一的数据写入的结果显示
+        /// </summary>
+        /// <param name="result">写入的结果信息</param>
+        /// <param name="address">地址信息</param>
+        public static void WriteResultRender(OperateResult result, string address)
+        {
+            if (result.IsSuccess)
+            {
+                MessageBox.Show(DateTime.Now.ToString("[HH:mm:ss] ") + $"[{address}] Write Success");
+            }
+            else
+            {
+                MessageBox.Show(DateTime.Now.ToString("[HH:mm:ss] ") + $"[{address}] Write Failed {Environment.NewLine} Reason：{result.ToMessageShowString()}");
+            }
+        }
+
+        /// <summary>
+        /// 统一的数据写入的结果显示
+        /// </summary>
+        /// <param name="result">写入的结果信息</param>
+        /// <param name="address">地址信息</param>
+        public static void WriteResultRender(Func<OperateResult> write, string address)
+        {
+            try
+            {
+                OperateResult result = write();
+                if (result.IsSuccess)
+                {
+                    MessageBox.Show(DateTime.Now.ToString("[HH:mm:ss] ") + $"[{address}] Write Success");
+                }
+                else
+                {
+                    MessageBox.Show(DateTime.Now.ToString("[HH:mm:ss] ") + $"[{address}] Write Failed {Environment.NewLine} Reason：{result.ToMessageShowString()}");
+                }
+            }
+            catch (Exception ex)
+            {
+                // 主要是为了捕获写入的值不正确的情况
+                MessageBox.Show("Data for writting is not corrent: " + ex.Message);
+            }
+        }
+
+        public static void BulkReadRenderResult(HslCommunication.Core.IReadWriteNet readWrite, TextBox addTextBox, TextBox lengthTextBox, TextBox resultTextBox)
+        {
+            try
+            {
+                OperateResult<byte[]> read = readWrite.Read(addTextBox.Text, ushort.Parse(lengthTextBox.Text));
+                if (read.IsSuccess)
+                {
+                    resultTextBox.Text = "Result：" + HslCommunication.BasicFramework.SoftBasic.ByteToHexString(read.Content);
+                }
+                else
+                {
+                    MessageBox.Show("Read Failed：" + read.ToMessageShowString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Read Failed：" + ex.Message);
+            }
+        }
     }
 }
